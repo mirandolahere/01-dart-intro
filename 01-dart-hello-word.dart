bb5af30ced9d8145fa8594975507d0cfe5c55fe1
Claudia Miranda Ch <externo340@compartamos.pe>
2023-05-24 14:02:31 -0500

    first commit

---
+++ 01-dart-hello-word.dart
@@ -0,0 +1,11 @@
+void main(){
+  
+  //var myName="Claudia";
+  //late final myName;
+  //String myName="Claudia";
+  const myName = "Claudia";
+  
+  print("Hola $myName");
+  print("Hola ${myName.toUpperCase()}");
+
+}
---
+++ 02_variables.dart
@@ -0,0 +1,27 @@
+void main(){
+  
+  final String pokemon = "Ditto";
+  final int hp=100;
+  final bool isAlive=true;
+  final List<String> abilities = ['impostor'];
+  final sprites = <String> ['ditto/front.png', 'ditto/back.png'];
+
+  dynamic errorMessage  = "Hola";
+  errorMessage  = true;
+  errorMessage  = [1,2,3,4];
+  errorMessage  = {1,2,3,4};
+  errorMessage  = "Hola";
+  errorMessage = () => true; // dynamic tbn puede ser igual a una funcion que regresa valor boleano
+  errorMessage = null;
+
+  print("""
+  $pokemon
+  $hp
+  $isAlive
+  $abilities
+  $sprites
+  $errorMessage
+  """);
+  
+  
+}
